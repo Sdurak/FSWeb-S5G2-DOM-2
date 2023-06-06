8b0907f4ebb26e5b354b71658cff3e6829f74d84
Sdurak <sdurak91@hotmail.com>
2023-06-06 17:40:34 +0300

    S5G2 Proje

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import './less/index.less'
 
 // Örnek bir event kullanımı aşağıdadır. Çalıştırmak için comment dışına alın
-// document.querySelector("h1").addEventListener("click",function(e){
-// alert("Bana tıkladın!")
-// });
 
+document.querySelector("h1").addEventListener("click",function(e){
+    alert("Bana tıkladın!")
+    });
 
 // Kodlar buraya gelecek!
+
+// document.querySelector("nav").addEventListener("mouseover", (event) => {
+//     alert ("Anasayfa koçum")
+//     });
+
+
+document.querySelectorAll("nav a").forEach((item) =>{
+    item.addEventListener("mouseout",(item) =>{
+    item.target.style.color = "#ff1493";
+    alert(item.target.innerText + " koçum");
+    });
+});
